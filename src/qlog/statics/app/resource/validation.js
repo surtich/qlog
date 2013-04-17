d1b5a5a4f1d778ca3bf6f68837ae1af5b9d0929f
apenedo <alejandro.penedo@intelygenz.com>
2013-04-17 10:14:40 +0200

    updated email validation regex

--- src/qlog/statics/app/resource/validation.js
+++ src/qlog/statics/app/resource/validation.js
@@ -1,7 +1,7 @@
 iris.resource(
 	function(self){
 
-		var emailPattern = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
+		var emailPattern = /^[_a-z0-9-]+(\.[_a-z0-9-]+)@[a-z0-9-]+(\.[a-z0-9-]+)(\.[a-z]{2,3})$/;
 		var urlPattern = /^(ftp|http|https):\/\/[^ "]+$/;
 
 		self.isEmail = function(txt){
